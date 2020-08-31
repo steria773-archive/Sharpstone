f9f382023148250a3d28c0a240df321374096dc5
Rabia Alhaffar <steria773@gmail.com>
2020-08-31 19:49:36 +0300

    Just some tweaks...

---
+++ Sharpstone/CommandList.cs
@@ -0,0 +1,29 @@
+﻿//Sharpstone Commands List File!!!
+//Copyright (c)2018 - 2019 , Rabia Alhaffar,All Rights Reserved!!!
+/*
+  WARNING:
+  NOTES:IF YOU ARE DO NOT KNOW C# OR YOU ARE NOT .NET C# DEVELOPER OR YOU ARE NOT EVEN A PROGRAMMER OR DEVELOPER BY MEAN,CONSULT REMOVING THIS TOOL!!!
+ */
+/*
+Commands List:
+
+System.Console.Write();
+System.Console.WriteLine();
+DateTime.Now();
+Console.Clear();
+System.Diagnostics.Process.Start();
+Console.BackgroundColor = ConsoleColor.Color;
+Console.ForegroundColor = ConsoleColor.Color; 
+Console.Beep();
+System.Media.SystemSounds.SystemSoundName.Play();
+Thread.Sleep();
+System.IO.File.Create();
+System.IO.File.Delete();
+
+Notes: These Functions Are Made By Me:
+
+System.Media.SoundPlayer.Stop();
+System.Media.SoundPlayer.Play(Media File Location);
+ 
+*/
+
--- Sharpstone/Program.cs
+++ Sharpstone/Program.cs
@@ -1,4 +1,4 @@
-//Copyright (c)2018 - 2019 , Rabia Alhaffar,All Rights Reserved!!!
+﻿//Copyright (c)2018 - 2019 , Rabia Alhaffar,All Rights Reserved!!!
 //Sharpstone Version 1.8.0.0,A C# .NET Command Line Created For C# Developers!!!
 using System;
 using System.CodeDom;
